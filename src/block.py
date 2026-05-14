9debc60f74095953d8cb8cb0d2a40f5e7e58fca7
vinniedotsys <vincent.hassen@laposte.net>
2026-05-14 22:26:48 +0200

    BlockType

--- src/block.py
+++ src/block.py
@@ -1,3 +1,14 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 
 def markdown_to_blocks(markdown):
@@ -9,3 +20,26 @@ def markdown_to_blocks(markdown):
             continue
         linelist.append(block)
     return linelist
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    
+    if re.match(r"^#{1,6} .+", block):
+        return BlockType.HEADING
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    if lines and all(re.match(r"^> ?.*$", line) for line in lines):
+        return BlockType.QUOTE
+    if lines and all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    is_ordered = True
+    for i, line in enumerate(lines, start=1):
+        m = re.match(r"^(\d+)\. .+$", line)
+        if not m or int(m.group(1)) != i:
+            is_ordered = False
+            break
+    if lines and is_ordered:
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+
--- src/test_block.py
+++ src/test_block.py
@@ -46,4 +46,16 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
-
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
